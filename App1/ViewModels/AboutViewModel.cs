4662fd3afefe7a501b7b0566ec21bc95cd6b2cbc
Wil <Wil@DESKTOP-L7HSPSV>
2023-11-30 15:19:31 -0400

    Add project files.

---
+++ App1/ViewModels/AboutViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace App1.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public AboutViewModel()
+        {
+            Title = "Morcegos";
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://media.tenor.com/ccQPQ5QiTjAAAAAC/kubby.gif"));
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
